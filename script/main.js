5cbb6e6a41fae09f40e6becafd45713b8baa6d2f
Nandrian <ranarkai@gmail.com>
2020-10-23 08:38:10 +0300

    all fixed

---
+++ script/main.js
@@ -0,0 +1,22 @@
+var maVoiture = document.querySelector('.voiture');
+var positionInitiale = maVoiture.style.left;
+let voitureRotate = false
+console.log('Voiture style: ', maVoiture.style);
+alert(maVoiture.style);
+
+function faireBouger(direction, rotation){  
+  positionInitiale = +positionInitiale + direction;
+    if(voitureRotate == false){
+      document.querySelector('.voiture').style.left = positionInitiale + 'px';
+  }
+    else{
+      document.querySelector('.voiture').style.top = positionInitiale + 'px'; 
+  }
+}
+
+/* var positionInitial = maVoiture.style.transform; */
+function faireTourner(){
+     var positionInitial = maVoiture.style.transform; 
+    document.querySelector('.voiture').style.transform = positionInitial + 'rotate(90deg)';
+    voitureRotate = !voitureRotate     
+  }
